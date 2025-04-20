f7a04e6c5b96880c1cafbcf3e605a29ffea0434d
DavidMcMorrow <mcmorrda@tcd.ie>
2025-04-20 11:39:36 +1000

    feat: styled the buttons and added a tooltip

--- src/app/components/dashboard/container.js
+++ src/app/components/dashboard/container.js
@@ -2,9 +2,13 @@
 
 import React from "react";
 import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css';
 
 const Dashboard = () => {
     const router = useRouter();
+    const [hovered, setHovered] = useState(null);
 
     const goToTicTacToe = () => {
       router.push('/tic-tac-toe');
@@ -19,14 +23,27 @@ const Dashboard = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
         height: "100vh",
       }}>
-        <button onClick={goToTicTacToe}>
+       
+        <button onClick={goToTicTacToe}
+        onMouseEnter={() => setHovered("TicTacToe")}
+        onMouseLeave={() => setHovered(null)}
+        className={`option-button ${hovered === "TicTacToe" ? "option-button-hover" : ""}`}>
             Go to TicTacToe
         </button>
-        <button onClick={goToSudoku}>
+      
+        <button 
+        data-tooltip-id="sudoku-tip" data-tooltip-content="Under Development"
+        onClick={goToSudoku}
+        onMouseEnter={() => setHovered("Sudoku")}
+        onMouseLeave={() => setHovered(null)}
+        className={`option-button ${hovered === "Sudoku" ? "option-button-hover" : ""}`}>
             Go to Sudoku
         </button>
+        <Tooltip id="sudoku-tip" place="bottom" />
+       
       </div>
     );
 };

1931820df16d6e98dbb4531cd1aeb7151a41b306
DavidMcMorrow <mcmorrda@tcd.ie>
2025-05-09 18:03:24 +1000

    updated the board

--- src/app/components/sudoku/container.js
+++ src/app/components/sudoku/container.js
@@ -1,15 +1,41 @@
+'use client'
+import Board from "./board";
+import { useState, useEffect } from "react";
+
 export default function container() {
+  
+  const [board, setBoard] = useState(
+    Array.from({ length: 9 }, () => Array(9).fill('1'))
+  );
+
+  function test(){
+    console.log("board", board);
+  }
+
   return ( 
-    <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        fontSize: "2rem",
-        fontWeight: "bold"
-      }}>
-        Under Development
-    </div>
+    <main style={styles.container}>
+      <button onClick={() => test()}> test</button>
+      <h1 className={'title'}>Sudoku</h1>
+      <Board board={board}/>
+    </main>
+    // <div style={{
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     height: "100vh",
+    //     fontSize: "2rem",
+    //     fontWeight: "bold"
+    //   }}>
+    //     Under Development
+    // </div>
     
  );
+}
+
+const styles = {
+  container: {
+    position: "relative",
+    textAlign: "center",
+    marginTop: "1.5rem",
+  },
 }

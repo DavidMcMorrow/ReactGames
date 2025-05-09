4f88da499262fce768240a43b1acc5c092aad8f6
DavidMcMorrow <mcmorrda@tcd.ie>
2025-05-09 20:10:59 +1000

    loaded the api's results into the board

--- src/app/components/sudoku/container.js
+++ src/app/components/sudoku/container.js
@@ -5,19 +5,40 @@ import { useState, useEffect } from "react";
 export default function Sudoku() {
   
   const [board, setBoard] = useState(
-    Array.from({ length: 9 }, () => Array(9).fill('1'))
+    Array.from({ length: 9 }, () => Array(9).fill(''))
+  );
+  const [original, setOriginal] = useState(
+    Array.from({ length: 9 }, () => Array(9).fill(''))
+  );
+  const [solution, setSolution] = useState(
+    Array.from({ length: 9 }, () => Array(9).fill(''))
   );
 
   function createMatrix(boardState){
+    let matrix = Array.from({ length: 9 }, () => Array(9).fill(''));
+    let line = -1;
     
+    for (let index = 0; index < boardState.length; index++) {
+      const element = boardState[index];
+      if(index % 9 == 0){
+        line++;
+      }
+      if(element == '0'){
+        matrix[line][index % 9] = '';
+      } else{
+        matrix[line][index % 9] = element
+      }
+    }
+    return matrix;
   }
 
   useEffect(() => {
     fetch("/api/sudoku")
     .then(res => res.json())
     .then(data => {
-      console.log(data);
-      setBoard(data.puzzle)
+      setBoard(createMatrix(data.puzzle));
+      setOriginal(board);
+      setSolution(createMatrix(data.solution));
     })
     .catch(err => console.error(err))
   }, [])

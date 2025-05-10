1f67b73331132df7b2e923894bae287d6f0b780b
DavidMcMorrow <mcmorrda@tcd.ie>
2025-05-10 12:22:40 +1000

    updated the board styles and allows the user to do inputs

--- src/app/components/sudoku/board/board.js
+++ src/app/components/sudoku/board/board.js
@@ -1,12 +1,21 @@
 import './board.css';
+import Cell from '../cell/cell';
 
-export default function Board({board}) {
+export default function Board({board, boardRef, originalBoard, selectedCell, setSelectedCell}) {
   return (
-    <div className="grid">
+    <div ref={boardRef} className="grid">
         {
           board.map((row, rowIndex) => 
             row.map((cell, colIndex) => (
-              <input className="cell" key={`${rowIndex}-${colIndex}`} value={cell || ''} maxLength={1} readOnly/>
+              <Cell
+                key={`${rowIndex}-${colIndex}`}
+                rowIndex={rowIndex}
+                colIndex={colIndex}
+                value={cell}
+                originalBoard={originalBoard[rowIndex][colIndex]}
+                selectedCell={selectedCell}
+                onClick={setSelectedCell} 
+              />
             ))
           )
         }
--- src/app/components/sudoku/container.js
+++ src/app/components/sudoku/container.js
@@ -1,18 +1,22 @@
 'use client'
 import Board from "./board/board";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Sudoku() {
   
   const [board, setBoard] = useState(
     Array.from({ length: 9 }, () => Array(9).fill(''))
   );
-  const [original, setOriginal] = useState(
+  const [originalBoard, setOriginalBoard] = useState(
     Array.from({ length: 9 }, () => Array(9).fill(''))
   );
   const [solution, setSolution] = useState(
     Array.from({ length: 9 }, () => Array(9).fill(''))
   );
+  const [selectedCell, setSelectedCell] = useState({row: null, col: null});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const boardRef = useRef();
 
   function createMatrix(boardState){
     let matrix = Array.from({ length: 9 }, () => Array(9).fill(''));
@@ -24,7 +28,7 @@ export default function Sudoku() {
         line++;
       }
       if(element == '0'){
-        matrix[line][index % 9] = '';
+        matrix[line][index % 9] = 0;
       } else{
         matrix[line][index % 9] = element
       }
@@ -32,21 +36,72 @@ export default function Sudoku() {
     return matrix;
   }
 
+  // Calls API
   useEffect(() => {
     fetch("/api/sudoku")
     .then(res => res.json())
     .then(data => {
       setBoard(createMatrix(data.puzzle));
-      setOriginal(board);
+      setOriginalBoard(createMatrix(data.puzzle));
       setSolution(createMatrix(data.solution));
+      setIsLoading(false);
     })
     .catch(err => console.error(err))
   }, [])
 
-  return ( 
+  // Inputs values
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      console.log("selectedCell", selectedCell);
+      
+      const {row, col} = selectedCell;
+      
+      if(row == null || col == null) return
+
+      if (originalBoard[row][col] !== 0) return;
+
+      if (e.key === 'Backspace' || e.key === 'Delete') {
+        const updated = board.map(r => [...r]);
+        updated[row][col] = 0;
+        setBoard(updated);
+        return;
+      }
+
+      if (/^[1-9]$/.test(e.key)) {
+        const updated = board.map(r => [...r]);
+        updated[row][col] = parseInt(e.key, 10);
+        setBoard(updated);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [selectedCell, board, originalBoard]);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if(!boardRef.current?.contains(e.target)){
+        setSelectedCell({row: null, col: null})
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  if (isLoading || !board || !originalBoard) {
+    return <p>Loading...</p>;
+  }
+
+  return (
     <main className="sudoku-container">
       <h1 className={'title'}>Sudoku</h1>
-      <Board board={board}/>
+      <Board 
+        board={board} 
+        boardRef={boardRef} 
+        originalBoard={originalBoard} 
+        selectedCell={selectedCell}
+        setSelectedCell={setSelectedCell}
+      />
     </main>
  );
 }
